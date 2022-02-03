f128b7744b138b398b20d38d6a2b50b9fbf230cc
Dominik Köppl <git@dkppl.de>
2022-02-03 17:01:58 +0900

    parallel exec.

--- run.sh
+++ run.sh
@@ -6,22 +6,29 @@ function die {
 	exit 1
 }
 
-
-make
-
-file=/yotta/pbwt/durbin.txt
-
-for threshold in 0.1 0.08 0.05 0.03 0.01; do
+function task {
 	echo "Threshold: $threshold"
 	# ./pbwt.x -t "$threshold" -i /yotta/pbwt/durbin.txt
-	./pbwt.x -i /yotta/pbwt/durbin.txt -t 0.1 -d $file.$threshold.divarray -h $file.$threshold.grammar.txt -m $file.$threshold.matrix -n $file.$threshold.intervals -w 2000
+	./pbwt.x -i /yotta/pbwt/durbin.txt -t "$threshold" -d $file.$threshold.divarray -h $file.$threshold.grammar.txt -m $file.$threshold.matrix -n $file.$threshold.intervals
+	#-w 2000
 	~/code/bigrepair/bigrepair $file.$threshold.matrix
 	~/code/shapedSlp/build/SlpEncBuild -i $file.$threshold.matrix -f Bigrepair -e ShapedSlpV2_Sd_SdMcl -o $file.$threshold.matrix.slp
 	~/code/bigrepair/bigrepair -i $file.$threshold.divarray
 	# ~/code/shapedSlp/build/SlpEncBuild -i $file.$threshold.divarray -f Bigrepair -e ShapedSlpV2_Sd_SdMcl -o $file.$threshold.divarray.slp
 	cargo run --manifest-path=transform_grammar/Cargo.toml -- -i $file.$threshold.grammar.txt  -o $file.$threshold.grammar.bin
 	~/code/shapedSlp/build/SlpEncBuild -i $file.$threshold.grammar.bin -f solca -e ShapedSlpV2_Sd_SdMcl -o $file.$threshold.grammar.slp
+}
+
+
+make
+
+file=/yotta/pbwt/durbin.txt
+mkdir -p log
+
+for threshold in 0.1 0.08 0.05 0.03 0.01; do
+		task 2>&1 | tee log/durbin.$threshold.log  &
 done
+wait
 
 
 
